4112939b239e8827bb758835c963f9813560b8d9
yuebuqun <1085838434@qq.com>
2023-12-26 20:11:55 +0800

    创建辟邪剑法

---
+++ src/main/java/com/atguigu/Bixiejianfa.java
@@ -0,0 +1,12 @@
+package com.atguigu;
+
+public class Bixiejianfa {
+    public static void main(String[] args) {
+        System.out.println("辟邪剑法");
+        System.out.println("辟邪剑法");
+        System.out.println("辟邪剑法");
+        System.out.println("辟邪剑法");
+        System.out.println("辟邪剑法");
+        System.out.println("辟邪剑法");
+    }
+}
